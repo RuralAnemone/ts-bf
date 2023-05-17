7651f25fe5672b60118035cf4e407e44352fae4f
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2023-05-17 21:29:04 +0000

    single character test

--- src/compiler/index.test.ts
+++ src/compiler/index.test.ts
@@ -3,8 +3,29 @@ import { compile } from './index'
 
 const boilerplate = "let memory = new Uint32Array(30000).fill(0);\nlet memoryPointer = 0;"
 
-describe('basic test', () => {
+describe('single character', () => {
 	test('+', () => {
 		expect(compile('+')).resolves.toBe(boilerplate + '\nmemory[memoryPointer]++;');
+	});
+	test('-', () => {
+		expect(compile('-')).resolves.toBe(boilerplate + '\nmemory[memoryPointer]--;');
+	});
+	test('>', () => {
+		expect(compile('>')).resolves.toBe(boilerplate + '\nmemoryPointer++;');
+	});
+	test('<', () => {
+		expect(compile('<')).resolves.toBe(boilerplate + '\nmemoryPointer--;');
+	});
+	test('.', () => {
+		expect(compile('.')).resolves.toBe(boilerplate + 'return String.fromCharCode(memory[memoryPointer]);');
+	});
+	test(',', () => {
+		expect(compile(',')).resolves.toBe(boilerplate + '\nmemory[memoryPointer] = prompt(\'Input: \').charCodeAt(0);');
+	});
+	test('[', () => {
+		expect(compile('[')).resolves.toBe(boilerplate + '\nwhile (memory[memoryPointer] != 0) {');
+	});
+	test(']', () => {
+		expect(compile(']')).resolves.toBe(boilerplate + '\n}');
 	});
 });
